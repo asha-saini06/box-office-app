e8405fbe000af4e2e3a80a4bf3f5fdee579ecf1d
Asha <sainiasha.ash@gmail.com>
2022-11-01 00:46:53 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Routes>
+      <Route exact path="/">
+        This is Home Page
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Routes>
+  );
 }
 
 export default App;
