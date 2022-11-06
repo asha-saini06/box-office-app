28727efbfc0d5ac9d69d94d5fed4c91b6a4bc9bc
Asha <sainiasha.ash@gmail.com>
2022-11-06 16:52:31 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/starred" element={<Starred />} />
+
+      <Route exact path="/show/:id" element={<Show />} />
+
       {/* <Route path="*" element={<PageNotFound />} /> */}
       {/* <Route>
           <div>Not Found</div>
