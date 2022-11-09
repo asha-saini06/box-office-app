fef873f83b3774e479c3dc6b116ccd5b3a2ffce2
Asha <sainiasha.ash@gmail.com>
2022-11-09 16:54:21 +0530

    defined theme for styled components and created a few styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/starred" element={<Starred />} />
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/starred" element={<Starred />} />
 
-      <Route exact path="/show/:id" element={<Show />} />
+        <Route exact path="/show/:id" element={<Show />} />
 
-      {/* <Route path="*" element={<PageNotFound />} /> */}
-      {/* <Route>
+        {/* <Route path="*" element={<PageNotFound />} /> */}
+        {/* <Route>
           <div>Not Found</div>
         </Route> */}
-    </Routes>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
